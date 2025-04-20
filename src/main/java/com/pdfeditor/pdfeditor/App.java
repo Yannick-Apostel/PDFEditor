70fd0c082be4a4275ea3ad5f118c76e98576c0dd
Yannick Apostel <apostelyannick@gmail.com>
2025-04-20 20:56:33 +0200

    added merge function

--- src/main/java/com/pdfeditor/pdfeditor/App.java
+++ src/main/java/com/pdfeditor/pdfeditor/App.java
@@ -1,11 +1,91 @@
 package com.pdfeditor.pdfeditor;
 
 import javafx.application.Application;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.pdfbox.cos.COSStream;
+import org.apache.pdfbox.io.IOUtils;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.io.RandomAccessStreamCache;
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
+import org.apache.pdfbox.pdfwriter.compress.CompressParameters;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+
 
 public class App extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
 
+    private Object IOUtils;
+
+    @Override
+    public void start(Stage stage) throws FileNotFoundException {
+        mergePdfFiles(stage);
+    }
+
+    private void mergePdfFiles(Stage stage) throws FileNotFoundException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("PDF-Dateien auswählen");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("PDF Dateien (*.pdf)", "*.pdf")
+        );
+
+        // Benutzer wählt mehrere Dateien aus
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(stage);
+
+        if (selectedFiles == null || selectedFiles.size() <= 1) {
+            System.out.println("Bitte wähle mindestens zwei PDF-Dateien aus.");
+            return;
+        }
+
+        // Speicherort abfragen
+        fileChooser.setTitle("Speicherort wählen");
+        fileChooser.setInitialFileName("zusammengefuegt.pdf");
+        File saveFile = fileChooser.showSaveDialog(stage);
+
+        if (saveFile == null) {
+            System.out.println("Speichern abgebrochen.");
+            return;
+        }
+
+
+
+        try (COSStream cosStream = new COSStream();
+             ByteArrayOutputStream mergedpdfOutput = new ByteArrayOutputStream())  {
+
+            PDFMergerUtility merger = new PDFMergerUtility();
+            merger.setDestinationFileName(saveFile.getAbsolutePath());
+
+            for (File file : selectedFiles) {
+                merger.addSource(file);
+            }
+            IOUtils StreamCacheCreateFunction = null;
+            merger.mergeDocuments(StreamCacheCreateFunction.createMemoryOnlyStreamCache(), CompressParameters.NO_COMPRESSION);
+
+            System.out.println("PDFs erfolgreich zusammengeführt!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Fehler beim Zusammenführen der PDFs.");
+        }
+    }
+    public static RandomAccessStreamCache.StreamCacheCreateFunction createMemoryOnlyStreamCache()
+    {
+        RandomAccessStreamCache.StreamCacheCreateFunction streamCache = null;
+        return streamCache;
+    }
+
+    
+    public static RandomAccessStreamCache.StreamCacheCreateFunction createTempFileOnlyStreamCache()
+    {
+        return MemoryUsageSetting.setupTempFileOnly().streamCache;
+    }
+
+
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,12 @@
 module com.pdfeditor.pdfeditor {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.pdfbox;
+    requires org.apache.pdfbox.io;
 
 
     opens com.pdfeditor.pdfeditor to javafx.fxml;
     exports com.pdfeditor.pdfeditor;
+    exports com.pdfeditor.pdfeditor.Controllers;
 
 }
